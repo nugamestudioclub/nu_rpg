94da92322a6824e5460c87fa87be2c0507d9687f
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2023-05-05 15:33:57 -0400

    added Exists to check if a point exists and TryLocate for no-exception location to ILocator interface

--- Assets/Scripts/Navigation/ILocator.cs
+++ Assets/Scripts/Navigation/ILocator.cs
@@ -1,5 +1,8 @@
 namespace NuRpg.Navigation {
 	public interface ILocator<P, D> {
+		bool Exists(P point);
+		bool Exists(P origin, D direction);
 		P Locate(P origin, D direction);
+		bool TryLocate(P origin, D direction, out P result);
 	}
 }
