891166ebe10a2325f2383ec15e05760f22a1dbf7
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2023-05-05 15:33:06 -0400

    added direction enums (arrow, cardinal)

---
+++ Assets/Scripts/Navigation/Direction.cs
@@ -0,0 +1,41 @@
+﻿namespace NuRpg.Navigation {
+	public enum ArrowDirection4 {
+		None,
+		Up,
+		Right,
+		Down,
+		Left,
+	}
+
+	public enum ArrowDirection8 {
+		None,
+		Up,
+		UpRight,
+		Right,
+		DownRight,
+		Down,
+		DownLeft,
+		Left,
+		UpLeft,
+	}
+
+	public enum CardinalDirection4 {
+		None,
+		North,
+		East,
+		South,
+		West,
+	}
+
+	public enum CardinalDirection8 {
+		None,
+		North,
+		Northeast,
+		East,
+		Southeast,
+		South,
+		Southwest,
+		West,
+		Northwest,
+	}
+}
